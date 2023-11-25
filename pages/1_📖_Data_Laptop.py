50d56ca97a0b5ba87cff62d5e6a936b4a23d3fe4
Hasda12 <143325859+Hasda12@users.noreply.github.com>
2023-11-25 14:53:55 +0700

    creat

--- pages/1_📖_Data_Laptop.py
+++ pages/1_📖_Data_Laptop.py
@@ -20,81 +20,80 @@ st.write("""
 
 st.write(db.get_laptop_data())
 
-data_categorization = db.read_categorization()
-
-st.markdown("# DATA CATEGORIZATION")
-
-ct_id = []
-ct_specification= []
-ct_criteria = []
-ct_class = []
-for data in data_categorization:
-    data_ct_id = data[0]
-    data_ct_specification = data[1]
-    data_ct_criteria = data[2]
-    data_ct_class = data[3]
-
-    ct_id.append(data_ct_id)
-    ct_specification.append(data_ct_specification)
-    ct_criteria.append(data_ct_criteria)
-    ct_class.append(data_ct_class)
-
-st.subheader("BACA DATA CATEGORIZATION")
-read_data_categorization = pd.DataFrame(data_categorization,columns=["id","specification","criteria", "class"])
-st.write(read_data_categorization)
-        
-st.subheader("Tambah Data Kategorisasi")
+st.subheader("Form Ubah Data Discrete Kriteria")
 col1, col2, col3 = st.columns(3)
 with col1:
-    create_id = st.text_input("id")
-    create_specification = st.text_input("specification")
+    title = st.text_input('Silahkan isi nama laptop?', '')
+    st.write('Nama Laptop :', title)
+
+    option = st.selectbox(
+        'Silahkan pilih brand laptop?',
+        ('HP', 'Dell', 'asus', 'lenovo', 'acer'))
+    st.write('You selected:', option)
+
+    option = st.selectbox(
+        'Silahkan pilih brand laptop?',
+        ('SSD', 'HDD', 'eMMc'))
+    st.write('You selected:', option)
+
+    title = st.text_input('Silahkan isi Durability', '')
+    if title.isdigit():
+        st.write('Nama Laptop:', title)
+    else:
+        st.warning('Input harus berupa angka.')
+
+    option = st.selectbox(
+        'Silahkan pilih Graphic Card',
+        ('AMD Radeon', 'Intel UHD', 'NVDIA', 'Apple M2'))
+    st.write('You selected:', option)
+
 with col2:
-    create_criteria = st.text_input("criteria")
-    create_class = st.text_input("class")
-if st.button("Tambah", type='primary'):
-    # Lakukan validasi input atau operasi lain jika diperlukan
-    db.create_categorization(create_id, create_specification, create_criteria, create_class)
-    st.success("Data Kategorisasi baru berhasil ditambah: id{}, specification={}, criteria={}, class={}".format(
-        create_id, create_specification, create_criteria, create_class))
-    st.rerun()
+    title = st.text_input('Masukan Harga terbaru', '')
+    if title.isdigit():
+        st.write('Harga Terbaru:', title)
+    else:
+        st.warning('Input harus berupa angka.')
 
-st.subheader("Ubah Data Kategorisasi")
-list_of_categrizations = [i[0] for i in data_categorization]
-selected_categorization = st.selectbox("Data Categorization", list_of_categrizations)
-categorization_result = db.get_categorization(selected_categorization)
+    option = st.selectbox(
+        'Silahkan pilih Processors',
+        ('HP', 'Dell', 'asus', 'lenovo', 'acer'))
+    st.write('You selected:', option)
 
-# Tampilkan data kategorisasi yang dipilih
-if categorization_result:
-    current_id = categorization_result[0][0]
-    current_specification = categorization_result [0][1]
-    current_criteria = categorization_result[0][2]
-    current_class = categorization_result[0][3]
+    title = st.text_input('Silahkan isi Besar RAM', '')
+    if title.isdigit():
+        st.write('Besar RAM:', title)
+    else:
+        st.warning('Input harus berupa angka.')
 
+    option = st.selectbox(
+        'Silahkan pilih Processors',
+        ('1920x1080', '1366x768'))
+    st.write('You selected:', option)
 
-    st.subheader("Form Ubah Data Kategorisasi")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        update_id = st.text_input("ID", value=current_id)
-        update_specification = st.text_input("Specification", value=current_specification)
-    with col2:
-        update_criteria = st.text_input("Criteria", value=current_criteria)
-    with col3:
-        update_class = st.text_input("Class", value=current_class)
+    option = st.selectbox(
+        'Silahkan pilih Size Screen',
+        ('12.5', '14.0', '15.6', '11.6'))
+    st.write('You selected:', option)
+with col3:
+    title = st.text_input('Masukan Harga Lama', '')
+    if title.isdigit():
+        st.write('Harga Lama:', title)
+    else:
+        st.warning('Input harus berupa angka.')
 
-    if st.button("Ubah", type='primary'):
-        # Lakukan validasi input atau operasi lain jika diperlukan
-        db.update_categorization(update_id, update_specification, update_criteria, update_class, current_id, current_specification, current_criteria, current_class)
-        st.success("Data Kategorisasi berhasil diubah: ID={}, Specification={}, Criteria={}, Class={}".format(update_id, update_specification, update_criteria, update_class))
-        st.rerun()
-else:
-    st.warning("Data Kategorisasi dengan ID {} tidak ditemukan.".format(update_id))
+    option = st.selectbox(
+        'Silahkan pilih Size Screen',
+        ('512', '1.000', '500', '32'))
+    st.write('You selected:', option)
+    
+    title = st.text_input('Masukan Berat Laptop', '')
+    if title.isdigit():
+        st.write('Berat Laptop:', title)
+    else:
+        st.warning('Input harus berupa angka.')
 
-st.subheader("Hapus Data Kategorisasi: {}".format(current_id))
-if st.button("Hapus", type='primary'):
-    db.delete_categorization(current_id, current_specification, current_criteria, current_class)
-    st.success("Data Kategorisasi berhasil dihapus: '{}'".format(current_id))
-    st.rerun()
-
+    title = st.text_input('Silahkan isi Link Laptop', '')
+    st.write('Link Laptop :', title)
 
 
 # try:
